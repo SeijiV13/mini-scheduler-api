70517ba17e5920125f1e9851d707a00897d70e48
Seiji Ralph Villafranca <seijiv13@outlook.com>
2021-04-13 16:52:30 +0800

    added cors security

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  // app.enableCors({origin: "http://localhost:4200"});
+  app.enableCors({origin: ["http://localhost:4200"]});
   app.use(helmet());
   await app.listen(process.env.PORT || 3000);
 }
